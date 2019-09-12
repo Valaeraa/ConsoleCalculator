73a7bd7fef4313a5f0d03a3c9b19ccc5149d8a39
Bruno <valaeraa@gmail.com>
2019-09-12 15:22:26 +0300

    Finished implementing a basic calculator.

--- ConsoleCalculator/ConsoleCalculator/ConsoleCalculator.cpp
+++ ConsoleCalculator/ConsoleCalculator/ConsoleCalculator.cpp
@@ -2,11 +2,36 @@
 //
 
 #include <iostream>
+#include "Calculator.h"
 
 using namespace std;
 
 int main()
 {
+	// Print to the console.
     cout << "Calculator Console Application\n";
 	cout << "Please enter the operation to perform. Format: a+b | a-b | a*b | a/b\n";
+
+	double x, y, result = 0.0;
+	char oper = '+';
+
+	Calculator calculator;
+
+	while (true)
+	{
+		// Receive input from the user.
+		cin >> x >> oper >> y;
+
+		if (oper == '/' && y == 0)
+		{
+			cout << "Division by 0 exception\n";
+			continue;
+		}
+		else
+		{
+			result = calculator.Calculate(x, oper, y);
+		}
+
+		cout << "Result is: " << result << "\n";
+	}
 }
