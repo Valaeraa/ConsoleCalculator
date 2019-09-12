206057a87923a1842c7eaff443db33a68606f32f
Bruno <valaeraa@gmail.com>
2019-09-12 15:09:23 +0300

    Implemented a Calculator class and a Calculate function.

--- ConsoleCalculator/ConsoleCalculator/ConsoleCalculator.cpp
+++ ConsoleCalculator/ConsoleCalculator/ConsoleCalculator.cpp
@@ -1,6 +1,12 @@
+// ConsoleCalculator.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
 #include <iostream>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Calculator Console Application\n";
+	cout << "Please enter the operation to perform. Format: a+b | a-b | a*b | a/b\n";
 }
